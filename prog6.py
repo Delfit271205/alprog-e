ae460c02fd6736a89496d021c3e06ee59f1f978c
M.ADEL FITRAH <madelfitrah690@gmail.com>
2024-11-08 10:03:05 +0800

    Add files via upload

---
+++ prog6.py
@@ -0,0 +1,30 @@
+#variabel assigment value
+x = 9
+y = 6
+print("nilai dari x adalah : " , y)
+
+# aturan penamaan variabel
+luas = 30 # bisa berupa kata
+adel_manis = 100 # jika dua kata, digabungb dengan "_"
+Fitrahx = 10 # boleh gtabung capital non-non kapital
+fitrahx = 5
+delfit88 = 10 # jika menggabungkan huruf dengan angka,angkanya di akhir
+print("nilai dari fitrah adalah : ", delfit88)
+
+
+a = 4 #integer bilangan bulat 
+
+print("tipe data dari a :",type(a))
+
+b = 7.8 # float bilangan pecahan
+print("tipe data dari b :",type(b))
+
+
+c = "gacor"#string gabungan karakter
+print("tipe data dari c :",type(c))
+
+
+d = false # bolean , true false
+print("tipe data dari d :",type(d))
+
+
