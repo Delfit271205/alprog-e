cfc9d3088c0098f824a1f01681c48d207933ba30
M.ADEL FITRAH <madelfitrah690@gmail.com>
2024-11-29 10:00:13 +0800

    Add files via upload

---
+++ prog8.py
@@ -0,0 +1,33 @@
+#aritmatika
+
+x=24
+y=53
+
+#operasi penjumlah
+hasil= x + y
+print ("hasil dari x + y = ",hasil)
+
+#operasi pengurangan
+hasil= x - y
+print ("hasil dari x - y = ",hasil)
+
+#operasi pembagian
+hasil= x / y
+print ("hasil dari x / y = ",hasil)
+
+#operasi perkalian
+hasil= x * y
+print ("hasil dari x * y = ",hasil)
+
+#exponen
+hasil= x ** y
+print ("hasil dari x ** y = ",hasil)
+
+#modulus
+hasil= x % y
+print ("hasil dari x % y = ",hasil)
+
+
+#floor division
+hasil= x // y
+print ("hasil dari x // y = ",hasil)
